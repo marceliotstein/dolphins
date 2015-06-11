268fe477c0c049d61de00bb8d0145c75f9d1a87c
Marc Eliot Stein <marc.eliot.stein@gmail.com>
2015-06-11 09:24:44 -0400

    getting ready for day two

---
+++ js/lpgdivorce.js
@@ -0,0 +1,71 @@
+/*!
+ * Start Bootstrap - Agency Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+ $(document).ready(function(e) {
+   apprtxt = lpgpractice['approach'];
+   $('#approach-text').html(apprtxt);
+
+   changetxt = lpgpractice['changing'];
+   $('#change-text').html(changetxt);
+
+   childrentxt = lpgpractice['children'];
+   $('#children-text').html(childrentxt);
+
+   stakestxt = lpgpractice['stakes'];
+   $('#stakes-text').html(stakestxt);
+
+   newyorktxt = lpgpractice['newyork'];
+   $('#newyork-text').html(newyorktxt);
+
+   landmarktxt = lpgpractice['landmark'];
+   $('#landmark-text').html(landmarktxt);
+
+   controltxt = lpgpractice['control'];
+   $('#control-text').html(controltxt);
+
+   teamtxt = lpgpractice['team'];
+   $('#team-text').html(teamtxt);
+
+   begintxt = lpgpractice['begin'];
+   $('#begin-text').html(begintxt);
+
+   larrybio = lpgbios['larry'];
+   $('#bio1').html(larrybio);
+
+   mikebio = lpgbios['mike'];
+   $('#bio2').html(mikebio);
+
+   scarlettbio = lpgbios['scarlett'];
+   $('#bio3').html(scarlettbio);
+ });
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+// Closes the Responsive Menu on Click outside Menu
+$('body > *').not('nav').click(function() {
+    if(!$('button.navbar-toggle').hasClass('collapsed')) {
+        $('.navbar-toggle:visible').click();
+    }
+});
